4cfcec767f39b5738e5f31c7e522c39553e811ea
Isaac Watt <isaacwatt@icloud.com>
2018-06-10 17:37:26 -0400

    List constructor fully defined

--- Data Structures/List.js
+++ Data Structures/List.js
@@ -7,21 +7,22 @@ List ADT implementation
 function List() {
   this.size = 0;
   this.data = []; // array of elements
+  this.position = 0; // index of current pos for iteration
+
   this.clear = clear;
   this.find = find;
+  this.prettyPrint = prettyPrint;
+  this.insert = insert;
   this.append = append;
-}
+  this.remove = remove;
+  this.front = front;
+  this.end = end;
+  this.prev = prev;
+  this.next = next;
+  this.length = length;
+  this.curPos = curPos;
+  this.moveTo = moveTo;
+  this.getElement = getElement;
+  this.contains = contains;
 
-function append(element) {
-  this.data[this.size++] = element;
-}
-
-
-function find(item) {
-  for (var  i = 0; i < this.size; ++i) {
-    if (this.data[i] === item) {
-      return i;
-    }
-  }
-  return -1;
 }

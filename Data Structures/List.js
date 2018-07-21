2c4c08b7e1f41a9cf43bb05d7a2f9de912eb3795
Isaac Watt <isaacwatt@icloud.com>
2018-07-21 12:44:32 -0400

    implements functions used for traversing our list

--- Data Structures/List.js
+++ Data Structures/List.js
@@ -11,7 +11,7 @@ function List() {
 
   this.clear = clear;
   this.find = find;
-  this.prettyPrint = prettyPrint;
+  this.prettyPrint = prettyPrint; // returns the list as a string
   this.insert = insert;
   this.append = append;
   this.remove = remove;
@@ -25,4 +25,98 @@ function List() {
   this.getElement = getElement;
   this.contains = contains;
 
+}
+
+/*
+  Method implementations
+*/
+
+// Append: adds an element to the list.
+function append(element) {
+  this.data[this.size] = element;
+  ++this.size;
+}
+
+function find(item) {
+  for (var  i = 0; i < this.size; ++i) {
+    if (this.data[i] === item) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+// Remove: removes an element from the list
+function remove(element) {
+  var elementIndex = this.find(element);
+  if (elementIndex >= 0) {
+    this.data.splice(elementIndex, 1);
+    --this.size;
+    return true;
+  }
+  return false;
+}
+
+// Length: returns the number of elements in the list
+function length() {
+  return this.length;
+}
+
+function prettyPrint() {
+  return this.data;
+}
+
+function insert(element, after) {
+  var insertPos = this.find(after);
+  if (insertPos > -1) {
+    this.data.splice(insertPos + 1, 0, element);
+    ++this.size;
+    return true;
+  }
+  return false;
+}
+
+function clear() {
+  delete this.data;
+  this.data = [];
+  this.size = this.position = 0;
+}
+
+function contains(element) {
+  for (var i = 0; i < this.size; ++i) {
+    if (this.data[i] == element) {
+      return true;
+    }
+  }
+  return false;
+}
+
+// Functions used for traversing our list 
+
+function front() {
+  this.pos = 0;
+}
+
+function end() {
+    this.pos = this.listSize - 1;
+}
+
+function prev() {
+  if (this.pos > 0) --this.pos;
+}
+
+function next() {
+  if (this.pos < this.listSize - 1) ++this.pos;
+}
+
+function currPos() {
+  return this.pos;
+}
+
+function moveTo(pos) {
+  this.pos = pos;
+}
+
+function getElement() {
+  return this.dataStore[this.pos];
 }
